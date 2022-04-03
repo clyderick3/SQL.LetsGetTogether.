5c9ee3358c5dff65af2afe0ab9d07a11c453c162
Clyde Broderick <clyderick3@gmail.com>
2022-04-03 12:00:17 -0400

    Updated old Students table for better compatibility with Enrollemnts.

---
+++ answers/exercise1.sql
@@ -0,0 +1,12 @@
+Updated Students table:
+mysql> SELECT * FROM Students;
++--------------+----------------+---------+------------+---------------+-----------+
+| StudentName  | Address        | City    | PostalCode | Country       | StudentID |
++--------------+----------------+---------+------------+---------------+-----------+
+| Mark Jackson | 88 Fifth Ln    | Trenton | 08618      | United States | NULL      |
+| Mara Johnson | 10 Twisted Ln  | Smyrna  | 19977      | United States | NULL      |
+| Mike Johnson | 12 Elephant Ln | Atlanta | 40466      | United States | NULL      |
+| Muna Bregee  | 40 Rephant Rd  | Eclipse | A4044      | Haiti         | NULL      |
+| Javale Magee | 5 Warrior Ave  | Java    | C2033      | Canada        | 1         |
++--------------+----------------+---------+------------+---------------+-----------+
+5 rows in set (0.01 sec)
