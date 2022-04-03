c87f69ee638f994efaa592e77ceb305c2e787e04
Clyde Broderick <clyderick3@gmail.com>
2022-04-03 12:46:49 -0400

    Corrected exercise 2.

--- answers/exercise2.sql
+++ answers/exercise2.sql
@@ -1,12 +1,11 @@
-mysql> SELECT Students.StudentID, Students.StudentName
-    -> FROM Students
-    -> INNER JOIN Enrollments
-    -> ON Students.StudentID = Enrollments.StudentID;
-+-----------+--------------+
-| StudentID | StudentName  |
-+-----------+--------------+
-| 3         | Mike Johnson |
-| 2         | Muna Bregee  |
-| 1         | Javale Magee |
-+-----------+--------------+
+mysql> SELECT * FROM Enrollments
+    -> INNER JOIN Students
+    -> on Enrollments.StudentID = Students.StudentID;
++-----------+----------+---------------+--------------+----------------+---------+------------+---------------+-----------+
+| StudentID | Enrolled | Country       | StudentName  | Address        | City    | PostalCode | Country       | StudentID |
++-----------+----------+---------------+--------------+----------------+---------+------------+---------------+-----------+
+| 3         | NO       | United States | Mike Johnson | 12 Elephant Ln | Atlanta | 40466      | United States | 3         |
+| 2         | YES      | Haiti         | Muna Bregee  | 40 Rephant Rd  | Eclipse | A4044      | Haiti         | 2         |
+| 1         | Yes      | Canada        | Javale Magee | 5 Warrior Ave  | Java    | C2033      | Canada        | 1         |
++-----------+----------+---------------+--------------+----------------+---------+------------+---------------+-----------+
 3 rows in set (0.01 sec)
