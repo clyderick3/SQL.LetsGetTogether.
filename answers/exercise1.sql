5503d6606bc8d6a04bb3ece64a08c78221e95886
Clyde Broderick <clyderick3@gmail.com>
2022-04-03 12:20:16 -0400

    Left joined students by StudentID. Completed e1.

--- answers/exercise1.sql
+++ answers/exercise1.sql
@@ -10,3 +10,17 @@ mysql> SELECT * FROM Students;
 | Javale Magee | 5 Warrior Ave  | Java    | C2033      | Canada        | 1         |
 +--------------+----------------+---------+------------+---------------+-----------+
 5 rows in set (0.01 sec)
+
+Created Enrollments.
+
+mysql> SELECT * FROM Enrollments
+    -> LEFT JOIN Students
+    -> ON Enrollments.StudentID = Students.StudentID;
++-----------+----------+---------------+--------------+----------------+---------+------------+---------------+-----------+
+| StudentID | Enrolled | Country       | StudentName  | Address        | City    | PostalCode | Country       | StudentID |
++-----------+----------+---------------+--------------+----------------+---------+------------+---------------+-----------+
+| 1         | Yes      | Canada        | Javale Magee | 5 Warrior Ave  | Java    | C2033      | Canada        | 1         |
+| 2         | YES      | Haiti         | Muna Bregee  | 40 Rephant Rd  | Eclipse | A4044      | Haiti         | 2         |
+| 3         | NO       | United States | Mike Johnson | 12 Elephant Ln | Atlanta | 40466      | United States | 3         |
++-----------+----------+---------------+--------------+----------------+---------+------------+---------------+-----------+
+3 rows in set (0.00 sec)
